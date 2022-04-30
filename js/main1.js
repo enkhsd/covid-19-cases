c7e70a3fcb4bd3e10137ed6ed99aea5bf0a2c247
enkhsd <enkhsd@uw.edu>
2022-04-30 01:33:36 -0700

    updates

--- js/main1.js
+++ js/main1.js
@@ -1,15 +1,14 @@
+// initialize basemmap
 mapboxgl.accessToken =
 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
-let map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 container: 'map', // container ID
-style: 'mapbox://styles/mapbox/dark-v10',
-zoom: 5, // starting zoom
-center: [138, 38] // starting center
+style: 'mapbox://styles/mapbox/light-v10', // style URL
+zoom: 3, // starting zoom
+center: [-100, 40], // starting center
+projection: 'albers'
 });
 
-const grades = [4, 5, 6],
-colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)'],
-radii = [5, 15, 20];
 
 //load data to the map as new layers.
 //map.on('load', function loadingData() {
@@ -17,82 +16,103 @@ map.on('load', () => { //simplifying the function statement: arrow with brackets
 
 // when loading a geojson, there are two steps
 // add a source of the data and then add the layer out of the source
-map.addSource('earthquakes', {
+map.addSource('rates', {
     type: 'geojson',
-    data: 'assets/earthquakes.geojson'
+    data: 'assets/rates.geojson'
 });
 
 map.addLayer({
-        'id': 'earthquakes-point',
-        'type': 'circle',
-        'source': 'earthquakes',
-        'minzoom': 5,
+        'id': 'rates-layer',
+        'type': 'fill',
+        'source': 'rates',
         'paint': {
             // increase the radii of the circle as the zoom level and dbh value increases
-            'circle-radius': {
-                'property': 'mag',
-                'stops': [
-                    [{
-                        zoom: 5,
-                        value: grades[0]
-                    }, radii[0]],
-                    [{
-                        zoom: 5,
-                        value: grades[1]
-                    }, radii[1]],
-                    [{
-                        zoom: 5,
-                        value: grades[2]
-                    }, radii[2]]
-                ]
-            },
-            'circle-color': {
-                'property': 'mag',
-                'stops': [
-                    [grades[0], colors[0]],
-                    [grades[1], colors[1]],
-                    [grades[2], colors[2]]
-                ]
-            },
-            'circle-stroke-color': 'white',
-            'circle-stroke-width': 1,
-            'circle-opacity': 0.6
+            'fill-color': [
+                'step',
+                ['get', 'rates'],
+
+                '#feebe2', // stop_output_0
+                21, // stop_input_0
+                '#fcc5c0', // stop_output_1
+                41, // stop_input_1
+                '#fa9fb5', // stop_output_2
+                61, // stop_input_2
+                '#f768a1', // stop_output_3
+                91, // stop_input_3
+                '#dd3497', // stop_input_4
+                131, // stop_input_4
+                '#ae017e', // stop_output_5
+                171, // stop_input_5
+                "#7a0177", // stop_output_6
+                201, // stop_input_6
+                "#b10026", // stop_output_7
+
+            ],
+            'fill-outline-color': '#BBBBBB',
+            'fill-opacity': 0.7,
         }
+
+
     },
-    'waterway-label'
+
 );
-
-
-// click on tree to view magnitude in a popup
-map.on('click', 'earthquakes-point', (event) => {
-    new mapboxgl.Popup()
-        .setLngLat(event.features[0].geometry.coordinates)
-        .setHTML(`<strong>Magnitude:</strong> ${event.features[0].properties.mag}`)
-        .addTo(map);
-});
-
-});
-
-
+const layers = [
+    '0-20',
+    '21-40',
+    '41-60',
+    '61-90',
+    '91-130',
+    '131-170',
+    '171-200',
+    '201 and more'
+];
+const colors = [
+    '#feebe270',
+    '#fcc5c070',
+    '#fa9fb570',
+    '#f768a170',
+    '#dd349770',
+    '#ae017e70',
+    '#7a017770',
+    '#b1002670'
+];
 // create legend
 const legend = document.getElementById('legend');
+legend.innerHTML = "<b>COVID-19 Rates" + source;
 
-//set up legend grades and labels
-var labels = ['<strong>Magnitude</strong>'], vbreak;
-//iterate through grades and create a scaled circle and label for each
-for (var i = 0; i < grades.length; i++) {
-vbreak = grades[i];
-// you need to manually adjust the radius of each dot on the legend 
-// in order to make sure the legend can be properly referred to the dot on the map.
-dot_radii = 2 * radii[i];
-labels.push(
-    '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
-    'px; height: ' +
-    dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
-    '</span></p>');
+layers.forEach((layer, i) => {
+    const color = colors[i];
+    const item = document.createElement('div');
+    const key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
 
-}
+    const value = document.createElement('span');
+    value.innerHTML = `${layer}`;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+});
+
+
+// click on tree to view a popup
+map.on('mousemove', ({
+    point
+}) => {
+    const rates = map.queryRenderedFeatures(point, {
+        layers: ['rates-layer']
+    });
+    document.getElementById('text-description').innerHTML = rates.length ?
+        `<p><strong><em>${rates[0].properties.county}</strong></em></p><p><strong><em>${rates[0].properties.rates}</strong> COVID-19 rates</em></p>` :
+        `<p>Hover over a county to view COVID-19 rates</p>`;
+});
+
+
+});
+// add the data source
 const source =
-'<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
 
+'<p style="text-align: left; font-size:10pt">Source: <a href="https://data.census.gov/cedsci/table?g=0100000US%24050000&d=ACS%205-Year%20Estimates%20Data%20Profiles&tid=ACSDP5Y2018.DP05&hidePreview=true">2018 ACS 5 year estimates</a></p>';
+
+// combine all the html codes.
 legend.innerHTML = labels.join('') + source;
